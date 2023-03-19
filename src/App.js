bf4b579efbece0dc862ce214de4f9007dd82a759
tapadar13 <monsurismyname@gmail.com>
2023-03-19 22:02:16 +0530

    refactored

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import axios from "axios";
 import config from "./constants/config";
+import axios from "axios";
 
 function App() {
   const [data, setData] = useState({});
